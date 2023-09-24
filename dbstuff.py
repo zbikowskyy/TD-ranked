858797d306b0cabfe36e949315f10266cc64c46f
CatEatingCardboardBox <109736521+zbikowskyy@users.noreply.github.com>
2023-09-24 12:06:22 +0200

    bingbong
    TDR manager

--- dbstuff.py
+++ dbstuff.py
@@ -3,6 +3,7 @@ import sqlite3 as sql
 connection = sql.connect ('Touchdownplayers.db')
 
 cursor = connection.cursor()
+
 def tablegracze():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS gracze (
@@ -23,13 +24,28 @@ def tablegraczesee():
     SELECT * FROM gracze
     """)
 
-
     for sublist in cursor.fetchall():
         for i, item in enumerate(sublist):
             print(str(item).ljust(20), end='')
         print()
 
-tablegraczesee()
+TDRM = {
+    1: tablegracze(),
+    2: cleargracze(),
+    3: tablegraczesee()
+}
+
+if __name__ == "__main__":
+    print("========== TDR management console ==========\n"
+          "1: Zregenerowanie tabeli statystyk graczy\n"
+          "2: Usuniecie wszystkiego z tabeli graczy\n"
+          "3: Pokazanie statystyk wszystkich graczy\n")
+    kmd = input("Komenda: ")
+
+    try:
+        TDRM[int(kmd)]
+    except:
+        print("Zla komenda")
 
 connection.commit()
 connection.close()
